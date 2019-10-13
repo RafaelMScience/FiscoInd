0c56a33fd206f33b55cd718b12a1f3803e516fad
RafaelM <RafaelM.Science@gmail.com>
2019-10-13 13:46:59 -0400

    commit

--- app/src/main/java/com/example/fiscon/SplashScreen.java
+++ app/src/main/java/com/example/fiscon/SplashScreen.java
@@ -2,6 +2,7 @@ package com.example.fiscon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SplashScreen extends AppCompatActivity {
@@ -9,6 +10,25 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+
+
+        final Intent i = new Intent(this,MainActivity.class);
+
+        Thread timer = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+
+                finally {
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+
+        timer.start();
     }
 }
